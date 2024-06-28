828689570bc7f14b5140afce0b709a792d5e89ed
inso.vs <xinaise.7ix@gmail.com>
2024-06-28 06:46:32 +0200

    Update Optimisation Services Medium.ps1

--- Optimisation Services Medium.ps1
+++ Optimisation Services Medium.ps1
@@ -2,17 +2,17 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 [System.Console]::Title = "[Services Optimization By inso.vs]"
 
 echo "Auteur: inso.vs
-Date de création: [28/06/2024] v0.1 (Version Optmimal)
+Date de crÃ©ation: [28/06/2024] v0.1 (Version Medium Safe)
 
 
 DESCRIPTION:
 
-- Ce script désactives les services Windows indésirables et inutiles pou une utilisation gaming/streaming.
+- Ce script dÃ©sactives les services Windows indÃ©sirables et inutiles pou une utilisation gaming/streaming.
 - Tout les services commentent leurs lignes correspondantes ci-dessous du script.
-- Si un de vos périphériques, app, ou quelques choses du genre ne s'exécute pas au demarrage,
-- Réactiver le en cherchant le/les potentiels services qui pourraient avoir eu effet sur son arrêt.
+- Si un de vos pÃ©riphÃ©riques, app, ou quelques choses du genre ne s'exÃ©cute pas au demarrage,
+- RÃ©activer le en cherchant le/les potentiels services qui pourraient avoir eu effet sur son arrÃªt.
 
-- Si le problème persiste, rejoignez mon Discord sur ma page 'guns.lol', où vous y serez rediriger à la fin du script.
+- Si le problÃ¨me persiste, rejoignez mon Discord sur ma page 'guns.lol', oÃ¹ vous y serez rediriger Ã  la fin du script.
 
 - n'oubliez pas de redemarrer le PC pour appliquer les Tweaks."
 Start-Sleep -Seconds 10
@@ -20,7 +20,7 @@ Start-Sleep -Seconds 10
 $confirmation = Read-Host "Veuillez ecrire 'ok' pour confirmer votre accord et demarrer le script. :"
 
 
-    Write-Host "Accord accepté. Le script continue."
+    Write-Host "Accord acceptÃ©. Le script continue."
 
 $services = @(
 
@@ -30,11 +30,11 @@ $services = @(
     "diagsvc"                                  # Diagnostics Tracking Service
     "dmwappushservice"                         # Service de routage de messages Push du protocole WAP (Wireless Application Protocol) de gestion des appareils
     "lfsvc"                                    # Geolocation Service
-    "MapsBroker"                               # Gestionnaire des cartes téléchargées
+    "MapsBroker"                               # Gestionnaire des cartes tÃ©lÃ©chargÃ©es
     "AMD External Events Utility"              # AMD External Events Utility             
     "AMD Crash Denfender Service"              # AMD Crash Denfender Service
     "edgeupdate"                               # Microsoft Edge       
-    "SCPolicySvc"                              # Stratégie de retrait de la carte à puce
+    "SCPolicySvc"                              # StratÃ©gie de retrait de la carte Ã  puce
     "SCardSvr"                                 # Carte a puce
     "BDESVC"                                   # Service de chiffrement de lecteur BitLocker
     "icssvc"                                   # Service Point d'acces sans fil mobile Windows
@@ -43,9 +43,9 @@ $services = @(
     "shpamsvc"                                 # Shared PC Account Manager
     "Themes"                                   # Themes    
     "WpcMonSvc"                                # Controle Parental
-    "VaultSvc"                                 # Gestionnaire d’informations d’identification
+    "VaultSvc"                                 # Gestionnaire dâ€™informations dâ€™identification
     "CscService"                               # Fichier hors connexion
-    "IKEEXT"                                   # Modules de génération de clés IKE et AuthIP                          
+    "IKEEXT"                                   # Modules de gÃ©nÃ©ration de clÃ©s IKE et AuthIP                          
     "TabletInputService"                       # Clavier tactile
     "UevAgentService"                          # Virtualization
     "WpnService"                               # Stylet
@@ -55,12 +55,12 @@ $services = @(
     "BcastDVRUserService"                      # GameDVR - Service utilisateur de diffusion et GameDVR
     "BcastDVRUserService_38117"                # GameDVR - Service utilisateur de diffusion et GameDVR_38117
     "NetTcpPortSharing"                        # Service de partage de ports Net.Tcp
-    "WerSvc"                                   # Service de rapport d’erreurs Windows
+    "WerSvc"                                   # Service de rapport dâ€™erreurs Windows
     "ScDeviceEnum"                             # Carte a puce
     "SensrSvc"                                 # Service de surveillance des capteurs
     "SensorService"                            # Service de capteur
     "DPS"                                      # Service de strategie de diagnostic
-    "PcaSvc"                                   # Service de l’Assistant Compatibilite des programmes
+    "PcaSvc"                                   # Service de lâ€™Assistant Compatibilite des programmes
     "WebClient"                                # WebClient
     "wisvc"                                    # Service Windows Insider
     "vmictimesync"                             # HYPER-V
@@ -78,37 +78,37 @@ $services = @(
     "tzautoupdate"                             # Programme de mise a jour automatique du fuseau horaire
     "Netlogon"                                 # Netlogon
     "SNMPTrap"                                 # Interruption SNMP
-    "WdiSystemHost"                            # Hôte système de diagnostics
-    "p2psvc"                                   # Groupement de mise en réseau de pairs
+    "WdiSystemHost"                            # HÃ´te systÃ¨me de diagnostics
+    "p2psvc"                                   # Groupement de mise en rÃ©seau de pairs
     "SEMgrSvc"                                 # Gestionnaires des paiements et des elements securises NFC
-    "RasAuto"                                  # Gestionnaire des connexions automatiques d’acces a distance
+    "RasAuto"                                  # Gestionnaire des connexions automatiques dâ€™acces a distance
     "WinRM"                                    # Gestion a distance de Windows (Gestion WSM)
     "MsKeyboardFilter"                         # Filtre clavier microsoft
-    "DusmSvc"                                  # Consommation des données
+    "DusmSvc"                                  # Consommation des donnÃ©es
     "wmiApSrv"                                 # Carte de performance WMI
     "PeerDistSvc"                              # BranchCache
-    "StiSvc"                                   # Acquisition d’image Windows (WIA)
+    "StiSvc"                                   # Acquisition dâ€™image Windows (WIA)
     "ShellHWDetection"                         # Detection materiel noyau
-    "FrameServer"                              # Serveur de trame de la Caméra Windows
-    "WdiSystemHost"                            # Hôte système de diagnostics
-    "WdiServiceHost"                           # Service hôte WDIServiceHost
-    "WinHttpAutoProxySvc"                      # Service de découverte automatique de Proxy Web pour les services HTTP Windows
+    "FrameServer"                              # Serveur de trame de la CamÃ©ra Windows
+    "WdiSystemHost"                            # HÃ´te systÃ¨me de diagnostics
+    "WdiServiceHost"                           # Service hÃ´te WDIServiceHost
+    "WinHttpAutoProxySvc"                      # Service de dÃ©couverte automatique de Proxy Web pour les services HTTP Windows
     "SSDPSRV"                                  # Decouverte SSDP
-    "FDResPub"                                 # Publication des ressources de découverte de fonctions
-    "SstpSvc"                                  # Service SSTP (Secure Socket Tunneling Protocol)     
+    "FDResPub"                                 # Publication des ressources de dÃ©couverte de fonctions
+    "SstpSvc"                                  # ServiceÂ SSTP (Secure Socket Tunneling Protocol)     
     "Eaphost"                                  # Protocole EAP (Extensible Authentication Protocol)
-    "RemoteAccess"                             # Routage et accès distant
-    "RemoteRegistry"                           # Registre à distance
-    "RetailDemo"                               # Service de démo du magasin
+    "RemoteAccess"                             # Routage et accÃ¨s distant
+    "RemoteRegistry"                           # Registre Ã  distance
+    "RetailDemo"                               # Service de dÃ©mo du magasin
     "SharedAccess"                             # Partage de connexion Internet (ICS)
-    "TrkWks"                                   # Client de suivi de lien distribué
-    "WbioSrvc"                                 # Service de biométrie Windows (requis pour le lecteur d'empreintes digitales / détection faciale)
-    "WMPNetworkSvc"                            # Service Partage réseau du Lecteur Windows Media
+    "TrkWks"                                   # Client de suivi de lien distribuÃ©
+    "WbioSrvc"                                 # Service de biomÃ©trie Windows (requis pour le lecteur d'empreintes digitales / dÃ©tection faciale)
+    "WMPNetworkSvc"                            # Service Partage rÃ©seau du Lecteur Windows Media
     #"wscsvc"                                  # Windows Security Center Service
     #"WSearch"                                 # Windows Search
     "ndu"                                      # Windows Network Data Usage Monitor
-    'TapiSrv'                                  # Téléphonie
-    'TrkWks'                                   # Client de suivi de lien distribué
+    'TapiSrv'                                  # TÃ©lÃ©phonie
+    'TrkWks'                                   # Client de suivi de lien distribuÃ©
     'MSiSCSI'                                  # Service Initiateur iSCSI de Microsoft
     # Services which cannot be disabled
     #"WdNisSvc"
